16084d5e22da6b2b5c5928e357d9add79471f51d
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-07-31 17:04:53 +0500

    starting

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template : "<h1>{{message}}</h1>",
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo';
+  message = "I am very excited";
 }
--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
