a7ccc163f92cfbd4d231a97a2b70c778d0c6dc8c
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-08-11 20:26:06 +0500

    components added

--- practice3/src/app/app-routing.module.ts
+++ practice3/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path: "", component: WelcomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
