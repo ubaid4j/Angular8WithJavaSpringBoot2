d78b5db1e69dd33bd8984abbe9d0fdcddce93641
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-08-16 22:48:37 +0500

    add

--- practice3/src/app/components/login/login.component.ts
+++ practice3/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { invalid } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,7 @@ export class LoginComponent implements OnInit
 {
     private name : string;
     private password : string;
+    private invalid : boolean = false;
 
     constructor()
     {
@@ -25,5 +27,14 @@ export class LoginComponent implements OnInit
     {
         console.log(this.name);
         console.log(this.password);
+        if(this.name != "test" || this.password != "test")
+        {
+            this.invalid = false;
+        }
+    }
+
+    private isInvalid() : boolean
+    {
+        return this.invalid;
     }
 }
