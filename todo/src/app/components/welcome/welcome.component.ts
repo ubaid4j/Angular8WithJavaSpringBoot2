f765a934d4ac3bf80d8651ba1a9c040277c912d6
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-08-21 18:58:41 +0500

    updated

--- todo/src/app/components/welcome/welcome.component.ts
+++ todo/src/app/components/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DataServiceService, HelloWorldBean } from 'src/app/services/data/data-service.service';
 
 @Component({
   selector: 'app-welcome',
@@ -10,7 +11,11 @@ export class WelcomeComponent implements OnInit
 {
     private name : string = '';
 
-    constructor(private route:ActivatedRoute)
+    private message : string;
+    private error : string;
+
+    constructor(private route:ActivatedRoute,
+        private dataService : DataServiceService)
     {
 
     }
@@ -18,6 +23,28 @@ export class WelcomeComponent implements OnInit
     ngOnInit()
     {
         this.name = this.route.snapshot.params['name'];
+    }
+
+    private getMessage() : string
+    {
+        console.log("Executing getMessage() at welcome component")
+        this.dataService.executeHelloWorldService().subscribe(
+            response => this.handleSuccessFulResponse(response),
+            error => this.handleError(error)
+        )
+        console.log("Ending getMessage() at welcome component");
+        return null;
+    }
+
+    private handleSuccessFulResponse(response : any) : void
+    {
+        this.message = response.message;
+        // console.log(response.message);
+    }
+
+    private handleError(error : any) : void
+    {
+        this.error = error.error.message;
     }
 
 }
