ef4a00886ec4eeb0e43737388f403918600852ec
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-08-21 16:30:03 +0500

    spring boot app add

---
+++ BootAngularApp/src/main/java/com/ubaid/BootAngularApp/BootAngularAppApplication.java
@@ -0,0 +1,15 @@
+package com.ubaid.BootAngularApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAngularAppApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(BootAngularAppApplication.class, args);
+	}
+
+}
---
+++ BootAngularApp/src/main/java/com/ubaid/BootAngularApp/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.ubaid.BootAngularApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ubaid.BootAngularApp.entity.HelloWorldBean;
+
+@RestController
+@RequestMapping("/")
+public class HomeController
+{
+	@GetMapping("helloWorld")
+	public String helloWorld()
+	{
+		return "Hello World";
+	}
+	
+	@GetMapping("helloWorldBean")
+	public HelloWorldBean hellowWorldBean()
+	{
+		return new HelloWorldBean("Hello World");
+	}
+}
