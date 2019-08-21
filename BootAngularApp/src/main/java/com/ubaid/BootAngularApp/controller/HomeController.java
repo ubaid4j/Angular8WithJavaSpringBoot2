d4bb650afd4c91a364586ddbea8f41a55e56c6b7
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-08-21 18:25:08 +0500

    @CrossOrigin add at rest handler

--- BootAngularApp/src/main/java/com/ubaid/BootAngularApp/controller/HomeController.java
+++ BootAngularApp/src/main/java/com/ubaid/BootAngularApp/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.ubaid.BootAngularApp.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import com.ubaid.BootAngularApp.entity.HelloWorldBean;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class HomeController
 {
 	@GetMapping("helloWorld")
