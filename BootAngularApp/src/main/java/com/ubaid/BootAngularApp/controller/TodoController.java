28cfeee600226ec6874eef6d54352b8c225886ff
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-08-22 10:50:08 +0500

    @Cross origin add

--- BootAngularApp/src/main/java/com/ubaid/BootAngularApp/controller/TodoController.java
+++ BootAngularApp/src/main/java/com/ubaid/BootAngularApp/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.ubaid.BootAngularApp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.ubaid.BootAngularApp.service.TService;
 
 @RestController
 @RequestMapping("users")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TodoController
 {
 	@Autowired
