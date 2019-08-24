9e8315ba703d304503c24a8f02a6cea028bc25c6
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-08-24 19:26:26 +0500

    Authentication bean add

---
+++ BootAngularApp/src/main/java/com/ubaid/BootAngularApp/entity/Authentication.java
@@ -0,0 +1,21 @@
+package com.ubaid.BootAngularApp.entity;
+
+public class Authentication
+{
+	private String message;
+	
+	public Authentication(String message)
+	{
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
