11e392a3e9379d6757938fcf988db46dd4b2811c
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-03-01 07:31:37 +0500

    gateway server and naming server add

---
+++ gateway-server/src/main/java/com/ubaid/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,17 @@
+package com.ubaid.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,20 @@
-<h2 align="center">Its all About Angular 8 and Spring Boot 2</h2>
+### About This Application
+- It has 
+ - 1 Microservice todo-serivce (BootAngularApp Directory)
+ - 1 Naming Server
+ - 1 API Gateway Server
+ - 1 Rest Client Server
+
+### todo-service
+- Connected with Oracle DB 19
+- Port
+  - 9000
+
+
+### naming-server
+- Port
+  - 8761
+
+### gateway-server
+- Port
+  - 8755
