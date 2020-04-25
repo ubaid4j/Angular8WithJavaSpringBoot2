0bbfce05edd5954b0fc5f78db03e101d95efd7e8
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-04-25 16:01:59 +0500

    automation complete

--- run.sh
+++ run.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-for d in ./*; do
-  if [[ -d $d ]] && [ "$d" != "BootAngularApp" ]; then
-    cd "$d" || exit;
-    mvn clean package -DskipTests;
-    cd ..;
-  fi;
-done;
+sudo docker-compose up --build;
